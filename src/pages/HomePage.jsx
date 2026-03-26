9947d00247ece551bfff878e917c584f3163d14a
pipersmyfurbae-hash <pipersmyfurbae@icloud.com>
2026-03-25 20:27:27 -0400

    Create HomePage.jsx landing page

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to Evercrafted!</h1>
+            <p>Your one-stop solution for all your crafting needs.</p>
+            <p>Explore our products and get inspired.</p>
+        </div>
+    );
+};
+
+export default HomePage;
