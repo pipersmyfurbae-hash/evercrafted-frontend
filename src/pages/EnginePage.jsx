1f3b4cae0daa51efe97a9ea41a67cdbcc7de255e
pipersmyfurbae-hash <pipersmyfurbae@icloud.com>
2026-03-25 20:26:23 -0400

    Create EnginePage.jsx for engine interface with iframe support.

---
+++ src/pages/EnginePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const EnginePage = () => {
+    return (
+        <div>
+            <h1>Engine Interface</h1>
+            <iframe 
+                src="https://example.com" 
+                width="100%" 
+                height="600px" 
+                title="Engine Interface"
+                style={{ border: 'none' }}
+            />
+        </div>
+    );
+};
+
+export default EnginePage;
